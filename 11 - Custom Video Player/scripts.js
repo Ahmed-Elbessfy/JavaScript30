07f53f28d6c5cda0af571bee02caf30e66fe73a8
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2021-02-12 17:48:07 +0200

    add play & pause functionality
    - add togglePlay function
    - toggelPlay function playes the video & pause it.
    - togglePlay function changes play btn symbol depeneding on video state
    - add eventlistener to fire togglePlay on playBtn clicked

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,3 +6,13 @@ skipBtns = document.querySelectorAll('[data-skip]'),
 progress = document.querySelector('.progress'),
 progressBar = document.querySelector('.progress__filled') 
 
+// Functions
+// play and pause the video & change play btn content
+const togglePlay = () => {
+    video.paused ? video.play() : video.pause()
+    playBtn.textContent = video.paused ?  '►' : '❚ ❚';
+} 
+
+// event listeners 
+// play and pause video
+playBtn.addEventListener('click', togglePlay)
