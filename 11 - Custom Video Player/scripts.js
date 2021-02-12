4dfee184d4cad5373dcced57691fb4e649bf4c67
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2021-02-12 17:42:44 +0200

    add variables
    - select html elements & store them in variables

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,8 @@
+// variables
+const video = document.querySelector('video'),
+playBtn = document.querySelector('.toggle'),
+ranges = document.querySelectorAll('.player__slider'),
+skipBtns = document.querySelectorAll('[data-skip]'),
+progress = document.querySelector('.progress'),
+progressBar = document.querySelector('.progress__filled') 
+
