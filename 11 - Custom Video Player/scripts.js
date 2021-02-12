23909fbebd00926cd94dae9336394331b8ffd68d
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2021-02-12 21:55:24 +0200

    add toggle view functionality
    - add toggle view button in Index.html file
    - add toggle view button style in style.css file
    - get toggle view button & video__player element and store them in variables
    - add toggleView function to toggle view to full screen and vice verse
    - add event listener to fire toggleView function on clicking on toggleViewBtn

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,9 @@ const video = document.querySelector('video'),
     ranges = document.querySelectorAll('.player__slider'),
     skipBtns = document.querySelectorAll('[data-skip]'),
     progress = document.querySelector('.progress'),
-    progressBar = document.querySelector('.progress__filled')
+    progressBar = document.querySelector('.progress__filled'),
+    toggleViewBtn = document.querySelector('.toggle__view'),
+    videoContainer = document.querySelector('.player__video')
 
 // to check if mouse is clicked while moving through progress bar
 let barClicked = false
@@ -43,6 +45,33 @@ const modifyRanges = (e) => {
     video[e.target.name] = e.target.value
 }
 
+// toggle view to full screen & vice verse
+/* if current video width is not equal the full width 
+    - modify video container width to be equal to window width
+    - modify video container height to be equal to window height
+    - modify .player element max width to be 100vw
+    - edit toggle view button to be Esc
+   if current video width is equal the full width 
+    - modify video container width to be equal to 100%
+    - modify video container height to be equal to be automatically determined
+    - modify .player element max width to be 750px as its default value
+    - edit toggle view button to be Full again
+*/
+const toggleView = () => {
+    if(parseInt(videoContainer.style.width.slice(0,-2)) != window.innerWidth){
+        videoContainer.style.width = window.innerWidth+'px'
+        videoContainer.style.height = window.innerHeight+'px'
+        document.querySelector('.player').style.maxWidth = '100vw'
+        toggleViewBtn.textContent = 'Esc'
+    } else {
+        videoContainer.style.width = '100%'
+        videoContainer.style.height = ''
+        document.querySelector('.player').style.maxWidth = '750px'
+        toggleViewBtn.textContent = 'Full'
+    }  
+}
+
+
 // event listeners 
 // play and pause video
 playBtn.addEventListener('click', togglePlay)
@@ -61,3 +90,5 @@ video.addEventListener('mousemove',(e) => barClicked && manuallyAdjustProgressBa
 progress.addEventListener('mousemove',(e) => barClicked && manuallyAdjustProgressBar(e))
 // adjust sound & speed
 ranges.forEach(range => range.addEventListener('change',modifyRanges))
+// toggle view 
+toggleViewBtn.addEventListener('click', toggleView)
