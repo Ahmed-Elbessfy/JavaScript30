58d2987797697ec8a92445a750171ae777c0d9f7
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2021-02-12 20:37:57 +0200

    add manually adjust video progress functionality
    - add barClicked variable
    - add manuallyAdjustProgressBar functionality
    - manuallyAdjustProgressBar modifies video current time depending on
    mouse position in progress bar
    - add event listeners mousedown, mouseup & mousemove to progress
    - add event listener mousemove to video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,17 +1,20 @@
 // variables
 const video = document.querySelector('video'),
-playBtn = document.querySelector('.toggle'),
-ranges = document.querySelectorAll('.player__slider'),
-skipBtns = document.querySelectorAll('[data-skip]'),
-progress = document.querySelector('.progress'),
-progressBar = document.querySelector('.progress__filled') 
+    playBtn = document.querySelector('.toggle'),
+    ranges = document.querySelectorAll('.player__slider'),
+    skipBtns = document.querySelectorAll('[data-skip]'),
+    progress = document.querySelector('.progress'),
+    progressBar = document.querySelector('.progress__filled')
+
+// to check if mouse is clicked while moving through progress bar
+let barClicked = false
 
 // Functions
 // play and pause the video & change play btn content
 const togglePlay = () => {
     video.paused ? video.play() : video.pause()
     playBtn.textContent = video.paused ?  '►' : '❚ ❚';
-} 
+}
 
 // skip forward/backward
 const skip = (e) => {
@@ -23,6 +26,12 @@ const skip = (e) => {
 const playProgress = () => {
     // modify progressBar width to be equal to how much played form the video
     progressBar.style.flexBasis = ((video.currentTime / video.duration) * 100) + '%'
+}
+// Manually adjust progress bar
+const manuallyAdjustProgressBar = (e) => {
+    // get approximate time depending where mouse position relative to progress bar width multiplying to total video duration
+    let clickedTime = (e.offsetX / progress.offsetWidth) * video.duration
+    video.currentTime = Math.floor(clickedTime)
 }
 
 // adjust sound & speed
@@ -42,5 +51,13 @@ video.addEventListener('click', togglePlay)
 skipBtns.forEach(btn => btn.addEventListener('click',skip))
 // progress bar auto adjustment
 video.addEventListener('timeupdate', playProgress)
+// Manually adjust progress bar
+// if mouse clicked and get hold inside progress bar, edit barClicked to be true
+progress.addEventListener('mousedown', () => barClicked = true)
+// if mouse not clicked inside progress bar, edit barClicked to be true
+progress.addEventListener('mouseup', () => barClicked = false)
+// fire manuallyAdjustProgressBar only if barClicked is true & mouse is moving inside both video or the progress bar
+video.addEventListener('mousemove',(e) => barClicked && manuallyAdjustProgressBar(e))
+progress.addEventListener('mousemove',(e) => barClicked && manuallyAdjustProgressBar(e))
 // adjust sound & speed
 ranges.forEach(range => range.addEventListener('change',modifyRanges))
