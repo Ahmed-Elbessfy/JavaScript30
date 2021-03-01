cfb5ce80e2ceb9ecd6eedaf7ac1e9a1020ed02f6
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2021-03-01 17:21:08 +0200

    get video from webcam
    - add getVideo function
    - call getVideo function for testing

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,24 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// functions
+// get user camera stream
+const getVideo = () => {
+  // console.log(navigator)
+  // get camera stream
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      // console.log(localMediaStream)
+      // set video src to camera stream
+      video.srcObject = localMediaStream
+      //play video
+      video.play()
+    })
+    .catch(
+      // if something went wrong or user denied camera access
+      err => console.log('Sorry! Something went wrong! ', err)
+    )
+}
+// test
+getVideo()
