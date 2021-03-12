aaa8066bef2c2dca38daffceb65a9d20f9fdaf2e
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2021-03-12 14:33:44 +0200

    display timer to user
    - add displayTimer function
    - in displayTimer: convert seconds to proper format
    - display proper time format in page title and in display timer html element

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -2,12 +2,24 @@
 // variable to store interval
 let countDown;
 // functions
+//display timer
+const displayTimer = s => {
+  // convert seconds to minutes:seconds format
+  const min = Math.floor(s / 60),
+    sec = s % 60
+  // display timer in display timer element
+  document.querySelector('.display__time-left').textContent = `${min}:${sec < 10 ? '0' + sec : sec}`
+  // display timer in page title
+  document.title = `${min}:${sec < 10 ? '0' + sec : sec}`
+}
 const timer = s => {
   // get current time in mille seconds
   const now = Date.now(),
     // end time in mille seconds
     then = now + s * 1000
 
+  // display timer once  timer function called since setInterval must wait for first second
+  displayTimer(s)
   countDown = setInterval(() => {
     // remaining seconds
     const secondsLeft = Math.round((then - Date.now()) / 1000)
@@ -20,7 +32,7 @@ const timer = s => {
       return
     }
     // display timer to user
-    console.log(secondsLeft)
+    displayTimer(secondsLeft)
   }, 1000)
 }
 
